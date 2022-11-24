1e79c0486c38057a07cf3712a2f6a31d91d8c42c
Olga <oevkarpova@gmail.com>
2022-11-24 20:20:39 +0300

    cors

--- frontend/src/App/api.ts
+++ frontend/src/App/api.ts
@@ -2,7 +2,7 @@ import Question from "../features/QuestionList/types/Question";
 
 
 export const loadQuestion = async (): Promise<Question[]> => {
-  const res = await fetch('http://localhost:4000/api');
+  const res = await fetch('http://localhost:4000/api', {credentials: 'include'});
   console.log(res)
   return res.json();
 };
