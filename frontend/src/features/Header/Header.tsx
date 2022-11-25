1866400f803efd448d3735d51df9b86b8ef9dedd
Yakov <sichevyakov@gmail.com>
2022-11-25 16:50:38 +0300

    fix header

--- frontend/src/features/Header/Header.tsx
+++ frontend/src/features/Header/Header.tsx
@@ -3,37 +3,49 @@ import { Outlet, NavLink, useNavigate } from 'react-router-dom';
 import './Header.css';
 import * as api from '../../App/api';
 import { Res } from '../Registration/types/User';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '../redux/store';
 
 
 
 function Header(): JSX.Element {
+  const { user } = useSelector((state: RootState) => state.userState);
   const navigate = useNavigate();
   const dispatch = useDispatch();
   function handleLogout(): void {
     console.log(1);
-    
-    api.logout().then((res:Res) => res.message === 'Session destroy' && dispatch({ type: 'LOGOUT' }));
+
+    api.logout().then((res: Res) => res.message === 'Session destroy' && dispatch({ type: 'LOGOUT' }));
     navigate('/');
   }
 
   return (
-      < div className="app__container">
-        <nav className="header__nav">
+
+    < div className="app__container">
+      <nav className="header__nav">
         <div className="header__container">
           <ul className="header__menu">
-            <li>
+            {user ? (
+              <>
+           <li>
               <NavLink className="header__item" to="/">
                 Главная
               </NavLink>
             </li>
-
             <li>
               <NavLink className="header__item" to="/results">
                 Результаты
               </NavLink>
-            </li><></>
-           <li>
+            </li>
+             <li>
+             <NavLink className="header__item" to="/" onClick={handleLogout}>
+               Выйти
+             </NavLink>
+           </li>
+           </>
+            ) : (
+              <>
+              <li>
               <NavLink className="header__item" to="/registration">
                 Регистрация
               </NavLink>
@@ -42,19 +54,17 @@ function Header(): JSX.Element {
               <NavLink className="header__item" to="/login">
                 Войти
               </NavLink>
-            </li>
-            <li>
-              <NavLink className="header__item" to="/" onClick={handleLogout}>
-                Выйти
-              </NavLink>
-            </li>
+            </li></>
+            )}
+
+
           </ul>
         </div>
       </nav>
-      </div>
-      
-    
-    
+    </div>
+
+
+
   );
 }
 
--- frontend/src/features/MainPage/MainPage.tsx
+++ frontend/src/features/MainPage/MainPage.tsx
@@ -1,19 +1,23 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 
 import { useNavigate } from 'react-router-dom';
+import { RootState } from '../redux/store';
 
 import './MainPage.css';
 
-function MainPage():JSX.Element {
+function MainPage(): JSX.Element {
+  const { user } = useSelector((state: RootState) => state.userState);
   const navigate = useNavigate();
   function onHandleNavigate(): void {
     navigate('/play');
   }
   return (
-<div className='fatherBtn'>
-    <button type="button" className="btn" onClick={onHandleNavigate}>Играть</button>
-</div>
+    <div className='fatherBtn'>
+      <button type="button" className="btn" onClick={onHandleNavigate}>Играть</button>
+    </div>
+
   );
 }
 
