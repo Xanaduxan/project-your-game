3f72f6110e7405169f97181d3832835125de99ce
Yakov <sichevyakov@gmail.com>
2022-11-25 10:46:32 +0300

    create rega logo

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,13 +3,17 @@ import { Route, Routes } from 'react-router-dom';
 
 import './App.css';
 import Header from './features/Header/Header';
+import Login from './features/Login/Login';
 import MainPage from './features/MainPage/MainPage';
+import Registration from './features/Registration/Registration';
 
 function App() {
   return (
     <Routes>
     <Route path="/" element={<Header />}>
       <Route path="main" element={<MainPage />} />
+      <Route path="registration" element={<Registration />} />
+      <Route path="login" element={<Login />} />
     </Route>
   </Routes>
   );
